f0a35bb993d9f63b9b5e2048af9d4f6e4e35f4f9
Gulkinator <lgulkus@gmail.com>
2015-06-13 09:06:06 -0500

    add inputs to create_lists migration file

--- db/migrate/20150613135511_create_lists.rb
+++ db/migrate/20150613135511_create_lists.rb
@@ -1,7 +1,8 @@
 class CreateLists < ActiveRecord::Migration
   def change
     create_table :lists do |t|
-
+	  t.string :title
+	  t.text :list_items
       t.timestamps null: false
     end
   end
