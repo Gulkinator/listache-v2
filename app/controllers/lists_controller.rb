9e2ad10161b242be19a1f50053e23f236e58d9aa
Gulkinator <lgulkus@gmail.com>
2015-06-13 10:50:11 -0500

    added before_action for add_item and delete_item to controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show, :edit, :update, :destroy]
+  before_action :set_list, only: [:show, :edit, :update, :destroy, :add_item, :delete_item]
 
   # GET /lists
   # GET /lists.json
